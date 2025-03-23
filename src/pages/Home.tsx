f77254385bce11afdccf710b2075d3366eb401e9
zhzkal2 <reliya1541@gmail.com>
2025-03-24 00:30:32 +0900

    feat :  complete page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -31,7 +31,7 @@ function Home() {
               alt="toranomon"
               width={642}
               height={61}
-              className="section-title-overlay__img"
+              className="section-title__overlay-img"
             />
 
             <h2>東京オフィスは、虎ノ門にて新しいスタートを切ります</h2>
@@ -50,8 +50,8 @@ function Home() {
             />
           </article>
 
-          <article className="section-subtitle__word">
-            <p className="section-subtitle-word__p ">
+          <article className="section-subtitle__text">
+            <p className="section-subtitle__text-p">
               このたび弊社は、東京オフィスを東京都港区虎ノ門に移転し
               ２０１４年３月４日(火)大安より営業の運びとなりました。
               新オフィスは、打ち合わせスペースも多くありますので
@@ -100,7 +100,7 @@ function Home() {
 
                 <div className=" article-section__beer">
                   {/* test text 부분과 교차 검증 필요 */}
-                  <span className="article-section__text">
+                  <span className="article-section__test-text">
                     83
                     <span className="article-section__text ">件</span>
                   </span>
@@ -134,7 +134,7 @@ function Home() {
                   />
                 </div>
               </div>
-              <div className="article-section__info mt-6 text-right text-[0.563rem] tracking-[0.028rem] text-[#FFFFFF] lg:mt-7">
+              <div className="article-section__info mt-6 text-right text-[0.563rem] tracking-[0.028rem] text-[#FFFFFF] lg:mt-5">
                 ※虎ノ門駅の半径1km以内
               </div>
             </article>
@@ -210,83 +210,82 @@ function Home() {
 
             {/* 5 col */}
             <article className="article-section__item article-section__item--5 bg-white p-6 shadow-[0px_1px_1px_#D8D8D8BF] lg:h-[33.125rem]">
-                <div className="article-section__title flex items-center gap-2">
-                  <img
-                    src="/Business-Graduation-cap-icon@2x.png"
-                    alt="Graduation_cap"
-                    width={44}
-                    height={30}
-                  />
-                  <span className="article-section__title-text text-[1.125rem] lg:text-[1.375rem]">
-                    虎ノ門の由来って？
-                  </span>
-                </div>
-
+              <div className="article-section__title flex items-center gap-2">
                 <img
-                  src="/photo@2x.png"
-                  alt="photo"
-                  width={450}
-                  height={320}
-                  className="article-section__contents-img mx-auto pt-4"
+                  src="/Business-Graduation-cap-icon@2x.png"
+                  alt="Graduation_cap"
+                  width={44}
+                  height={30}
                 />
-                <span className="article-section__contents-text block pt-8 text-[0.675rem] leading-[1.25rem] lg:text-[0.8rem]">
-                  虎ノ門駅を出てすぐ、
-                  <br className="lg-hidden" />{" "}
-                  虎ノ門交差点付近に鎮座するこの銅像。
-                  <br />
-                  これ、なんだかお分かりでしょうか？ <br />
-                  そう、猫です。というのは嘘で予想どおり、
-                  <br className="lg-hidden" /> 虎です(笑) <br />
-                  虎ノ門だけに虎の銅像なんです！ <br />
-                  そもそもどうして、
-                  <br className="lg-hidden" />{" "}
-                  ここの地名が「虎ノ門」なのか気になりませんか？
+                <span className="article-section__title-text text-[1.125rem] lg:text-[1.375rem]">
+                  虎ノ門の由来って？
                 </span>
-              
+              </div>
+
+              <img
+                src="/photo@2x.png"
+                alt="photo"
+                width={450}
+                height={320}
+                className="article-section__contents-img mx-auto pt-4"
+              />
+              <span className="article-section__contents-text block pt-8 text-[0.675rem] leading-[1.25rem] lg:text-[0.8rem]">
+                虎ノ門駅を出てすぐ、
+                <br className="lg-hidden" />
+                虎ノ門交差点付近に鎮座するこの銅像。
+                <br />
+                これ、なんだかお分かりでしょうか？ <br />
+                そう、猫です。というのは嘘で予想どおり、
+                <br className="lg-hidden" /> 虎です(笑) <br />
+                虎ノ門だけに虎の銅像なんです！ <br />
+                そもそもどうして、
+                <br className="lg-hidden" />
+                ここの地名が「虎ノ門」なのか気になりませんか？
+              </span>
             </article>
+
             {/* 6 col */}
             <article className="article-section__item article-section__item--6">
-         
+              <img
+                src="/Pick up foods@2x.png"
+                alt="food"
+                width={253}
+                height={42}
+                className="article-section__title"
+              />
+              <div className="article-section__contents">
                 <img
-                  src="/Pick up foods@2x.png"
-                  alt="food"
-                  width={253}
-                  height={42}
-                  className="article-section__title"
+                  src="/ramen-photo@2x.png"
+                  alt="ramen-photo"
+                  width={161}
+                  height={204}
+                  className="article-section__contents-img"
                 />
-                <div className="article-section__contents flex flex-col items-center justify-center gap-4 pt-6 lg:flex-row lg:items-start">
-                  <img
-                    src="/ramen-photo@2x.png"
-                    alt="ramen-photo"
-                    width={161}
-                    height={204}
-                    className="article-section__contents-img mx-auto"
-                  />
-                  <span className="article-section__contents-text text-center lg:text-left">
-                    <span className="block text-[1.125rem] tracking-[-0.028125rem] text-white">
-                      めん徳　二代目つじ田　新橋店
-                      <br />
-                      <br className="hidden xl:block" />
-                    </span>
+                <div className="article-section__contents-container ">
+                  <span className="article-section__contents-text1">
+                    めん徳　二代目つじ田　
+                    <br className="block md-hidden" />
+                    新橋店
+                    <br className="hidden xl-block" />
+                  </span>
 
-                    <span className="mt-4 block text-[0.725rem] tracking-[-0.01875rem] text-white lg:text-[0.7rem] xl:mt-0 xl:text-[0.75rem]">
-                      1月24日にオープンしたばかりのラーメン店。
-                      <br />
-                      看板メニューのつけ麺は3段階に進化します。 <br />
-                      最初はそのまま、途中ですだちを絞って爽やかに、 <br />
-                      最後は黒七味をかけてピリっとした風味を楽しんだ <br />
-                      ところで美味しく完食…。といった流れです。 <br />
-                      <br className="hidden xl:block" />
-                    </span>
+                  <span className="article-section__contents-text2">
+                    1月24日にオープンしたばかりのラーメン店。
+                    <br />
+                    看板メニューのつけ麺は3段階に進化します。 <br />
+                    最初はそのまま、途中ですだちを絞って爽やかに、 <br />
+                    最後は黒七味をかけてピリっとした風味を楽しんだ <br />
+                    ところで美味しく完食…。といった流れです。
+                    <br className="hidden xl-block" />
+                  </span>
 
-                    <span className="mt-4 block text-[0.725rem] tracking-[-0.01875rem] text-white lg:text-[0.7rem] xl:mt-0 xl:text-[0.75rem]">
-                      弊社スタッフ達も、
-                      <br className="lg:hidden" />
-                      頻繁に出没しそうな予感がして います。
-                    </span>
+                  <span className="article-section__contents-text2">
+                    弊社スタッフ達も、
+                    <br className="" />
+                    頻繁に出没しそうな予感がして います。
                   </span>
                 </div>
-              
+              </div>
             </article>
           </section>
         </section>
@@ -307,9 +306,9 @@ function Home() {
                   height={36}
                   className="section-link-button-box__img"
                 />
-                <p className="hidden md-block section-link-button-box__buttonText">
+                <p className="hidden sm-block section-link-button-box__buttonText">
                   株式会社アンクシステムズ
-                  <br className="hidden md-block lg-hidden" />
+                  <br className="hidden sm-block lg-hidden" />
                   コーポレートサイトへ
                 </p>
               </div>
@@ -322,67 +321,6 @@ function Home() {
               />
             </div>
           </Link>
-        </section>
-
-        <section className="test-section  relative mx-auto mt-4 flex w-full max-w-[1000px] flex-wrap justify-center">
-          {/* /* 1 Col /} */}
-          <article className="article-section__item article-section__item--4 ">
-            <div className="article-section__title  ">
-              <img
-                src="/iPhone@2x.png"
-                alt="iphone_icon"
-                width={19}
-                height={26}
-              />
-              <span className="article-section__title-text ">
-                電話・FAX番号が変わりました
-              </span>
-            </div>
-
-            <img
-              src="/tel-fax@2x.png"
-              alt="toranomon"
-              width={385}
-              height={80}
-              className="article-section__contents"
-            />
-          </article>
-          <article className="article-section__item article-section__item--4 ">
-            <div className="article-section__title  ">
-              <img
-                src="/iPhone@2x.png"
-                alt="iphone_icon"
-                width={19}
-                height={26}
-              />
-              <span className="article-section__title-text ">
-                電話・FAX番号が変わりました
-              </span>
-            </div>
-
-            <img
-              src="/tel-fax@2x.png"
-              alt="toranomon"
-              width={385}
-              height={80}
-              className="article-section__contents"
-            />
-          </article>
-
-          {/* {/ 2 Col /} */}
-          <article className="blue flex max-w-[500px] flex-shrink-1 grow-0 basis-[500px] bg-[#DC5E48] shadow-[0px_1px1px#D8D8D8BF]">
-            {/* {/ content /} */}
-          </article>
-
-          {/* {/ 3 Col /} */}
-          <article className="black flex max-w-[500px] flex-shrink-1 grow-0 basis-[500px] bg-[#E9BC06] p-4 shadow-[0px_1px1px#D8D8D8BF]">
-            {/* {/ content /} */}
-          </article>
-
-          {/* {/ 4 Col /} */}
-          <article className="green flex max-w-[500px] flex-shrink-1 grow-0 basis-[500px] bg-[#846AB4] p-4 shadow-[0px_1px1px#D8D8D8BF]">
-            {/* {/ content/}  */}
-          </article>
         </section>
       </main>
       <Footer />
