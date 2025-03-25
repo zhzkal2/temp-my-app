020071b94ee70c4f8f76284863835208ac593cb9
zhzkal2 <reliya1541@gmail.com>
2025-03-25 21:48:35 +0900

    fix vercel.json --5

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
 function App() {
   return (
-    <>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-    </>
+    </HashRouter>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+
 import "./styles/index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>
 );
