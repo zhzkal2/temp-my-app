5913c219d2905f29114d8e0317d7475f0c7b1266
zhzkal2 <reliya1541@gmail.com>
2025-03-26 00:04:12 +0900

    fix : App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,7 @@
-import { useState, useEffect } from "react";
 import { HashRouter, Routes, Route } from "react-router-dom";
-import Preloader from "./Components/Preloader";
 import Home from "./pages/Home";
 
 function App() {
-  // const [loading, setLoading] = useState(true);
-
-  // useEffect(() => {
-  //   // 2초 후에 로딩 상태를 변경하여 프리로더를 숨깁니다.
-  //   setTimeout(() => {
-  //     setLoading(false);
-  //   }, 3000);
-  // }, []);
-
   return (
     <HashRouter>
       <Routes>
